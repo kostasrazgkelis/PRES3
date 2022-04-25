a9ae828a31d3d4c801fa12a03cd9ddb6623d1fd5
KoRaZ21 <konstantinos.razgkelis@onelity.com>
2022-04-25 15:29:32 +0300

    cleaned code

--- services/data-service-1/app.py
+++ services/data-service-1/app.py
@@ -1,107 +1,45 @@
-from flask import Flask, json, send_file, request
+"""
+    The backend endpoints of our web application for the service A (Alice)
+"""
+from flask import Flask, send_file, request
 import os
 from flask.wrappers import Response
-import jellyfish
 import pandas as pd
-import random
-import hashlib
-import requests
-import numpy as np
+import packages.etl_pipeline as etl
 
 HOST = '0.0.0.0'
 PORT = '9200'
 NAME_OF_CLUSTER = "Cluster_A"
-test_path = f"C:/Users/Kostas Razg/Desktop/Πτυχιακή/MyThesis/myThesis/services/data-service-1/book_chapter_table_25p_200k_A.csv"
-correct_path = f'/var/lib/data/datafile.csv'
 
 app = Flask(__name__)
 
-
-def create_alp():  
-    return str(chr(random.randrange(65,90))) + str(chr(random.randrange(48,54))) + str(chr(random.randrange(48,54))) + str(chr(random.randrange(48,54)))
-
-def create_noise(noise, size, name):
-    return pd.Series([create_alp() for _ in range(int(noise * size / 100))], dtype = 'string', name=name)
-
-def create_fake_index(noise,size, name):
-    return pd.Series(['Fake Index' for _ in range(int(noise * size / 100))], dtype = 'string', name=name)
-
-def create_fake_soundex_values(noise, size, columns):
-    a = create_noise(noise, size, columns[1])
-    b = create_noise(noise, size, columns[2])
-    fake_index = create_fake_index(noise, size, columns[0])
-
-    return pd.DataFrame(pd.concat([fake_index, a, b], axis=1))
-
-@app.route('/')
-def get():
-    return Response(200)
-
-@app.route("/take_data/", methods=["POST"])
 @app.route("/take_data/<matching_field>/", methods=['GET'])
 @app.route("/take_data/<matching_field>/<int:noise>", methods=["GET"])
-def post(matching_field=None, noise=None):
+def get(matching_field=None , noise=0):
+    """ The main get function that starts the ETL pipeline and return to Carol the encrypted data
 
-    columns_we_care = ['NCID', 'last_name', 'first_name']
-    if request.method == 'GET' and 0 <= noise <= 1000:
+    Returns:
+        http_response: The file with the encrypted data
+    """
 
-        """
-        Start Extracting the data 
-        """
-        data_frame = pd.read_csv(correct_path, header=0, delimiter=';"', engine='python').astype('string')
+    if  1000 <= noise <= 0:
+        return None
 
+    etl_object = etl.ETLModel(matching_field, noise)
+    etl_object.start_etl()
+    return send_file(f'/var/lib/data/hidden_information.csv',
+                    mimetype='text/csv',
+                    attachment_filename='a_cluster_data.csv',
+                    as_attachment=True)
 
-        """
-        Finished Extracting the data 
-        """
-
-        """
-        Start Transforming the data 
-        """
-        for column in data_frame.columns:
-            data_frame.rename({column : column.strip('"')}, axis=1, inplace=True)
-
-        
-        for field in data_frame.columns:
-            if field not in columns_we_care:
-                data_frame.drop(columns = [field], inplace=True)
-            else:
-                data_frame[field] = data_frame[field].str.strip('"')
-
-        columns_we_care.remove(matching_field)
-
-        fake_soundex_values = create_fake_soundex_values(noise, data_frame.shape[0], data_frame.columns)
-
-        for column in columns_we_care:
-            #Applies to the data the jellyfish-soundex function and also encodes them with the SHA256 encryption
-            data_frame[column] = data_frame[column].apply(lambda x: jellyfish.soundex(str(x))).apply(lambda x: hashlib.sha256( x.encode()).hexdigest() )
-
-            #Creates fake soundex values
-            fake_soundex_values[column] = fake_soundex_values[column].apply(lambda x: hashlib.sha256( x.encode()).hexdigest() )
-
-        #We merge all the fake soundex data with the correct 
-        merged_data = pd.concat([data_frame, fake_soundex_values], axis=0).sort_values(by=column)
-        merged_data.to_csv('/var/lib/data/hidden_information.csv', encoding='utf-8', header=True, index=False)
-        #We send back the final merged file
-
-        """
-        Finished Transforming the data 
-        """
-                
-        return send_file(f'/var/lib/data/hidden_information.csv',
-                        mimetype='text/csv',
-                        attachment_filename='a_cluster_data.csv',
-                        as_attachment=True)
-
-
-    elif request.method == 'POST':
-
-        try:
-            downloaded_data = pd.read_csv(request.files['file'])
-            downloaded_data.to_csv('/var/lib/data/datafile.csv', encoding='utf-8', index=False)  
-        except FileExistsError as e:
-            return Response(f"We could not find the file!")
-        return Response(f"Save the file")
+@app.route("/take_data/", methods=["POST"])
+def post():
+    try:
+        downloaded_data = pd.read_csv(request.files['file'])
+        downloaded_data.to_csv('/var/lib/data/datafile.csv', encoding='utf-8', index=False)  
+    except FileExistsError:
+        return Response(f"We could not find the file!")
+    return Response(f"Save the file")
 
 
 if __name__ == '__main__':
---
+++ services/data-service-2/packages/etl_pipeline.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import packages.transformation_functions as tf
+import jellyfish
+import hashlib
+
+correct_path = f'/var/lib/data/datafile.csv'
+
+class ETLModel:
+    """
+        This is the ETL class model that will be used in backend for our application.
+        The ETL pipeline is rensponsible for the excraction, transformation and loading
+        the data.
+    """
+    def __init__(self, matching_field : str, noise : int): 
+        self.matching_field = matching_field
+        self.noise = noise
+
+    def extract_data(self) -> pd.DataFrame:
+        """ This function exctracts the data from a source location. (e.g csv file)
+
+        Returns:
+            pd.DataFrame: A pandas dataframe
+        """
+        self.dataframe = pd.read_csv(correct_path, header=0).astype('string')
+
+    def transform_data(self) -> pd.DataFrame:
+        """ This function take the exctracted dataframe and starts transforming the data (cleans the data)
+
+        Args:
+            matching_field (str): the column which will be used to make the join operation to find our matches
+            noise (int): The percentage of the noise we are going to use in our dataset (e.g. 10%, 20%)       
+            dataframe (pd.DataFrame): The dataframe with the data
+            
+        Returns:
+            pd.DataFrame: The transformed data 
+        """
+        columns_we_care = ['NCID', 'last_name', 'first_name', 'midl_name', 'street_name' , 'res_city_desc' , 'birth_age']
+        columns_we_care.remove(self.matching_field)
+
+        fake_soundex_values = tf.create_fake_soundex_values(self.noise, self.dataframe, self.matching_field)
+
+        for column in columns_we_care:
+            #Applies to the data the jellyfish-soundex function and also encodes them with the SHA256 encryption
+            self.dataframe[column] = self.dataframe[column]\
+                                    .apply(lambda x: jellyfish.soundex(str(x)))\
+                                    .apply(lambda x: hashlib.sha256( x.encode()).hexdigest() )
+
+            #Creates fake soundex values
+            fake_soundex_values[column] = fake_soundex_values[column].apply(lambda x: hashlib.sha256( x.encode()).hexdigest() )
+
+        #We merge all the fake soundex data with the correct 
+        merged_data = pd.concat([self.dataframe, fake_soundex_values], axis=0).sort_values(by='first_name')
+        merged_data.reset_index(drop=True, inplace=True)
+
+        self.dataframe = merged_data
+
+    def load_data(self):
+        """ Loads the data to a dumpfile (csv file) and stores them in the storage inside the container
+
+        Args:
+            dataframe (pd.DataFrame): _description_
+        """
+        self.dataframe.to_csv('/var/lib/data/hidden_information.csv', encoding='utf-8', header=True, index=False)
+
+    def start_etl(self):
+        """ Start the etl pipeline
+        """
+        self.extract_data()
+        self.transform_data()
+        self.load_data()
