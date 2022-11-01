709a45354f3ed8e2992d70d4c30de2ecc398cd47
KoRaZ21 <konstantinos.razgkelis@onelity.com>
2022-11-01 13:01:11 +0200

    THESIS-04: Fetch data from backend

--- src/services/front/src/App.js
+++ src/services/front/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import ShowFiles from "./components/ShowFiles/ShowFiles.js";
 import UploadFiles from "./components/UploadFiles/UploadFiles.js";
 import Home from './components/Home/Home';
@@ -29,8 +29,9 @@ function App() {
     }
   ]);
   const [filesA, setFilesA] = useState(null);
-  const [filesB, setFilesB] = useState(null);
-
+  const [joinedFiles, setJoinedFiles] = useState(null);
+  const [matchedFiles, setMatchedFiles] = useState(null);
+  const [transformedFiles, setTransformedFiles] = useState(null);
   const uploadCompletedFiles = () => {
     /* State */
 
@@ -43,7 +44,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/all-files" element={<ShowFiles filesA={filesA} setFilesA={setFilesA}/>} />
-          <Route path="/hdfs" element={<HdfsShowFiles filesA={filesA} setFilesA={setFilesA} filesB={filesB} setFilesB={setFilesB} />} />
+          <Route path="/hdfs" element={<HdfsShowFiles joinedFiles={joinedFiles} setJoinedFiles={setJoinedFiles} matchedFiles={matchedFiles} setMatchedFiles={setMatchedFiles} transformedFiles={transformedFiles} setTransformedFiles={setTransformedFiles}/>} />
           <Route path="/upload-files" element={<UploadFiles/>} />
           <Route path="/request-matched-data" element={<RequestMatchedData/>} />
         </Routes>
--- src/services/front/src/components/HdfsShowFiles/ShowFiles.js
+++ src/services/front/src/components/HdfsShowFiles/ShowFiles.js
@@ -10,121 +10,160 @@ const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-export default function HdfsShowFiles({filesA, setFilesA, filesB, setFilesB}) {
+export default function HdfsShowFiles({joinedFiles, setJoinedFiles, matchedFiles, setMatchedFiles, transformedFiles, setTransformedFiles}) {
 
-    const [properties, setProperties] = useState({});
     const [loadingJoin, setLoadingJoin] = useState(false);
     const [results, setResults] = useState(null);
     const [open, setOpen] = useState(false);
-    const [files, setFiles] = useState(null);
+    const [files, setFiles] = useState(null)
     const NAME_OF_CLUSTER = process.env.REACT_APP_NAME_OF_CLUSTER;
     const NAME_OF_CLUSTER_URL = process.env.REACT_APP_NAME_OF_CLUSTER_URL;
 
-    const filesRes = {
-        "joined_data": ["joined_files.csv"],
-        "matched_data": ["matched_files.csv"],
+    const filesRes = [
+    {
+        "data": {
+            "files": [
+                {
+                    "columns": [
+                        "NCID",
+                        "last_name",
+                        "first_name",
+                        "midl_name",
+                        "street_name",
+                        "res_city_desc",
+                        "birth_age"
+                    ],
+                    "name": "joined_files_1.csv"
+                },
+                {
+                    "columns": [
+                        "NCID",
+                        "last_name",
+                        "first_name",
+                        "midl_name",
+                        "street_name",
+                        "res_city_desc",
+                        "birth_age"
+                    ],
+                    "name": "joined_files_2.csv"
+                },
+            ]
+        }
+    },
+    {
+        "data": {
+            "files": [
+                {
+                    "columns": [
+                        "NCID",
+                        "last_name",
+                        "first_name",
+                        "midl_name",
+                        "street_name",
+                        "res_city_desc",
+                        "birth_age"
+                    ],
+                    "name": "matched_file_1.csv"
+                },
+                {
+                    "columns": [
+                        "NCID",
+                        "last_name",
+                        "first_name",
+                        "midl_name",
+                        "street_name",
+                        "res_city_desc",
+                        "birth_age"
+                    ],
+                    "name": "matched_file_2.csv"
+                },
+            ]
+        }
+    },
+    {
+        "data": {
+            "files": [
+                {
+                    "columns": [
+                        "NCID",
+                        "last_name",
+                        "first_name",
+                        "midl_name",
+                        "street_name",
+                        "res_city_desc",
+                        "birth_age"
+                    ],
+                    "name": "transformed_file_1.csv"
+                },
+                {
+                    "columns": [
+                        "NCID",
+                        "last_name",
+                        "first_name",
+                        "midl_name",
+                        "street_name",
+                        "res_city_desc",
+                        "birth_age"
+                    ],
+                    "name": "transformed_file_2.csv"
+                },
+            ]
+        }
     }
-
-    const MockJoinedFiles = [
-            {"file_name": "joined_files_1.csv"},
-            {"file_name": "joined_files_2.csv"},
-        ]
-
-    const MockMatchedFiles = [
-            {"file_name": NAME_OF_CLUSTER_URL + "_matched_files_1.csv"},
-            {"file_name": NAME_OF_CLUSTER_URL + "_matched_files_2.csv"},
-        ]
-
-    const MockTransformedFiles = [
-            {"file_name": NAME_OF_CLUSTER_URL + "_pretransformed_files_1.csv"},
-            {"file_name": NAME_OF_CLUSTER_URL + "_pretransformed_files_2.csv"},
-        ]
-
+]
     useEffect(() => {
         fetchFiles();
     }, []);
 
-    const fetchFiles = async() => {
+    const fetchFiles = async () => {
         try{
-            axios.all([getJoinedFileFromHDFS(),
-                       getMatchedAFromHDFS()]).then(
+            await axios.all([getJoinedFileFromHDFS(),
+                             getMatchedAFromHDFS(),
+                             getPretransformedAFromHDFS()]).then(
                 axios.spread((...allData) => {
-                    const allJoinedData = allData[0].data
-                    const allMatchedData = allData[1].data
-                    setFiles(filesRes)
-
-            })
-        )
-
+                    setFiles(allData)
+                })
+            )
         }catch(error){
-            setFilesA(MockJoinedFiles)
-            setFilesB(MockMatchedFiles)
+            setFiles(filesRes)
             console.log('ERROR ', error);
         }
-      
+
     }
     useEffect(() => {
-        if(!files) return;
+        if (!files) return;
 
-        let a = files['joined_data'];
-        let b = files['matched_data'];
+        let joined_data_array = [];
+        let matched_data_array = [];
+        let transformed_data_array = [];
 
-        let FAarr = [];
-        let FBarr = [];
-
-        /* Manipulation of A files */
-        for(let el of a){
-            let obj = {}
-            obj.name = el.name;
-            FAarr.push(obj);
-        }
-
-        for(let el of b){
-            let obj = {}
-            obj.name = el.name;
-            FBarr.push(obj);
-        }
-        setFilesA(FAarr)
-        setFilesB(FBarr)
-
-    }, []);
-
-
-    const handeProperties = (event) =>{
-        const { name, value } = event.target;
-        console.log('HANDLE CHANGES ', name, value);
-        var obj = {...properties, [name]: value}
-        setProperties(obj)
-    }
-
-    const onFileSelectedA = (event) => {
-        const { name, checked } = event.target;
-
-        const newArr = filesA.map((obj) => {
-            if (obj.name === name) {
-                return {...obj, selected: checked};
+        if (files[0].data.files.length !== 0)
+            for(let el of files[0].data.files){
+                let obj = {}
+                obj.name = el.name;
+                joined_data_array.push(obj);
             }
-            else{
-                return {...obj, selected: false};
-            }
-        });
-        setFilesA(newArr);
-    }
 
-    const onFileSelectedB = (event) => {
-        const { name, checked } = event.target;
-
-        const newArr = filesB.map((obj) => {
-            if (obj.name === name) {
-                return {...obj, selected: checked};
+        if (files[1].data.files.length !== 0)
+            for(let el of files[1].data.files){
+                let obj = {}
+                obj.name = el.name;
+                matched_data_array.push(obj);
             }
-            else{
-                return {...obj, selected: false};
-            }
-        });
 
-        setFilesB(newArr);
-    }
+        if (files[2].data.files.length !== 0)
+            for(let el of files[2].data.files){
+                let obj = {}
+                obj.name = el.name;
+                transformed_data_array.push(obj);
+            }
+
+        setJoinedFiles(joined_data_array)
+        setMatchedFiles(matched_data_array)
+        setTransformedFiles(transformed_data_array)
+
+    }, [files]);
+
 
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
@@ -146,11 +185,11 @@ export default function HdfsShowFiles({filesA, setFilesA, filesB, setFilesB}) {
         }
     };
 
-    const displayJoinedFiles = MockJoinedFiles?.map((item, idx) => {
+    const displayJoinedFiles = joinedFiles?.map((item, idx) => {
     return (
         <div className={styles.FileWrapper} key={idx}>
             <div className={styles.FileItem} >
-                <p>{item.file_name}</p>
+                <p>{item.name}</p>
                 <div>
                     <button className={styles.MarginTopXSmall} onClick={() => {
                         handleClickDownload("joined_data", item.file_name)}}>Download</button>
@@ -160,11 +199,11 @@ export default function HdfsShowFiles({filesA, setFilesA, filesB, setFilesB}) {
         )
     })
 
-    const displayMatchedData = MockMatchedFiles?.map((item, idx) => {
+    const displayMatchedData = matchedFiles?.map((item, idx) => {
     return (
         <div className={styles.FileWrapper} key={idx}>
             <div className={styles.FileItem} >
-                <p>{item.file_name}</p>
+                <p>{item.name}</p>
                 <div>
                     <button className={styles.MarginTopXSmall} onClick={() => {
                         handleClickDownload(NAME_OF_CLUSTER_URL + "_matched_data", item.file_name)}}>Download</button>
@@ -174,11 +213,11 @@ export default function HdfsShowFiles({filesA, setFilesA, filesB, setFilesB}) {
         )
     })
 
-    const displayTransformedData = MockTransformedFiles?.map((item, idx) => {
+    const displayTransformedData = transformedFiles?.map((item, idx) => {
     return (
         <div className={styles.FileWrapper} key={idx}>
             <div className={styles.FileItem} >
-                <p>{item.file_name}</p>
+                <p>{item.name}</p>
                 <div>
                     <button className={styles.MarginTopXSmall} onClick={() => {
                         handleClickDownload("_pretransformed_data", item.file_name)}}>Download</button>
